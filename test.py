5c391c9891745629edf45db3e9461ee0004401d4
GabrieLima-dev <81888850+GabrieLima-dev@users.noreply.github.com>
2021-10-08 16:42:17 -0300

    Delete test.py

--- test.py
+++
@@ -1,5 +0,0 @@
-quadrado = int(input("Digite o valor correspondente ao lado de um quadrado: "))
-perimetro = str(4 * quadrado)
-area = str(2 * quadrado)
-
-print("Perímetro: " + perimetro + " Área: " + area)
